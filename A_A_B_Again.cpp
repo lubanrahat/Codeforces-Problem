5bb3eb4ebd55cb8d14c87c8072256801f93a3afb
Luban Rahat <lubanrahat.dev@gmail.com>
2025-08-10 15:27:57 +0600

    Sun Aug 10

---
+++ A_A_B_Again.cpp
@@ -0,0 +1,22 @@
+/*
+   Author:  Luban Rahat
+   Created: 2025-08-10 14:21:37 (GMT+06:00)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    while(t--) {
+        string n; cin >> n;
+
+        cout << n[0] - '0' + n[1] - '0' << endl;
+
+    }
+
+    return 0;
+}
